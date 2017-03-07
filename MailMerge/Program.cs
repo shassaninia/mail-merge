a934cb759decb5bf5b11ad57025a9f0f297257b0
shassaninia <xai.soft80@gmail.com>
2017-03-06 22:20:39 -0500

    Mail Merge code

--- MailMerge/Program.cs
+++ MailMerge/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Word;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,50 @@ namespace MailMerge
     {
         static void Main(string[] args)
         {
+            var docFields = new Dictionary<string, string>()
+            {
+                {"FirstName","Saied" },
+                {"LastName", "Hassaninia" }
+            };
+
+            var application = new Application();
+            var document = new Document();
+
+            document = application.Documents.Add(@"c:\mail-merge\template.docx");
+
+            for (int i = 0; i < 1000; i++)
+            {
+
+
+                foreach (Field field in document.Fields)
+                {
+                    var fieldCode = field.Code;
+                    var fieldText = fieldCode.Text;
+
+                    if (fieldText.StartsWith(" MERGEFIELD"))
+                    {
+                        var endMerge = fieldText.IndexOf("\\");
+                        var fieldNameLength = fieldText.Length - endMerge;
+                        var fieldName = fieldText.Substring(11, endMerge - 11);
+
+                        fieldName = fieldName.Trim();
+
+                        foreach (var item in docFields)
+                        {
+                            if (fieldName == item.Key)
+                            {
+                                field.Select();
+                                application.Selection.TypeText(item.Value);
+                            }
+                        }
+                    }
+                }
+                document.SaveAs2(@"c:\mail-merge\saved" + i + ".docx");
+            }
+            
+
+            document.Close();
+            application.Quit();
         }
     }
 }
